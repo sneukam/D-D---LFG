e4a949872e94f7eb6ae9288a4bde8d9867ec006e
neukams <neukams@oregonstate.edu>
2021-02-22 18:06:37 -0500

    Update index.py
    Database connection is now working, and the example data populates in the console when run. Put in some basic GET route handlers that serve up pages - most of these routes have not been tested yet save the login page (from previous iterations of this file)

--- index.py
+++ index.py
@@ -1,18 +1,72 @@
 from flask import Flask, render_template, json
 import os
-import database.db_connector as db
+# import database.db_connector as db    # not sure why 'database' is there.
+import db_connector as db
 
-# Configuration
-
+# Flask
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0     # tells the browser to pull a refresh from the server if 0 seconds have passed.
+
+# Database Connection
 db_connection = db.connect_to_database()
 
-# Routes 
-
+# Routes
 @app.route('/')
 def root():
-    # return render_template("login.html")
-    return render_template("views/login.js")
+    return render_template("login.html")
+
+@app.route('/login')
+def login():
+    #TODO: Test credentials, create session
+    return render_template("login.html")
+
+@app.route('/sign-up')
+def signup():
+    return render_template("signup.html")
+
+@app.route('/characters')
+def signup():
+    return render_template("characters.html")
+
+@app.route('/create-campaign')
+def create_campaign():
+    return render_template("create-campaign.html")
+
+@app.route('/available-campaigns')
+def available_campaign():
+    return render_template("available-campaign.html")
+
+@app.route('/availability')
+def availability():
+    return render_template("availability.html")
+
+@app.route('/account')
+def account():
+    return render_template("account.html")
+
+@app.route('/logout')
+def logout():
+    # TODO: erase their session
+    return render_template("login.html")
+
+# TODO: CREATE + READ Operations
+
+# TODO: POST: Sign Up page - Create new user
+
+# TODO: POST: Login page - Redirect to new page if the credentials are correct, collect/distribute session cookies.
+
+# TODO: GET: Account page - serve w/ data
+
+# TODO: GET: 404 page not found
+
+
+
+
+
+# TODO: UPDATE + DELETE Operations
+# ...
+
+
 
 @app.route('/bsg-people')
 def bsg_people():
