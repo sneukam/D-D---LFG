53622ff9a5559ddb6452b1dc7dbbd17c028298c4
neukams <neukams@oregonstate.edu>
2021-02-28 18:46:12 -0500

    Update availability.js
    Page fully handles user interactions with backend

--- static/js/availability.js
+++ static/js/availability.js
@@ -4,55 +4,95 @@
 	
 **************************************/
 
-window.addEventListener('DOMContentLoaded', function () {	
-	document.getElementById("availability-submit-button").addEventListener("click", function(){
-		event.preventDefault();
-		alert("Updated availability will be sent to the db via a POST request\n\nWebpage is static at the moment so you will not be automatically removed from the campaign on the left-hand table... yet");
-	});
-});
-
-function call_day_availability_update() {
-	// this could be more clean... better design to put in a loop that gets the days and checked/unchecked values and passes it into the update function.
-	update('monday');
-	update('tuesday');
-	update('wednesday');
-	update('thursday');
-	update('friday');
-	update('saturday');
-	update('sunday');
+function get_post_url() {
+	return 'http://flip3.engr.oregonstate.edu:6735/availability'
 }
 
-function update(day_) {
-	var available;
-	var username;
-	var user_id;	// we can get this via the handlebars hidden tag method, sessions?, or query the DB right now. Affects Post code below.
+function get_availability() {
+	// returns the user's availability as a JSON object.
+	// ex: {monday:1, tuesday:0, wednesday:1, ...}
 	
-	// query to get the user's ID?
+	var availability = {monday:null, tuesday:null, wednesday:null, thursday:null, friday:null, saturday:null, sunday:null};
+	var checkboxes = document.getElementsByClassName("availability_checkboxes");
+	for (let i=0; i<checkboxes.length; i++) {
+		let day = checkboxes[i];
+		if (day.checked == true) {
+			availability[day.name] = 1;
+		}
+		else if (day.checked == false) {
+			availability[day.name] = 0;
+		}
+	}
+	return availability;
+}
+
+function get_campaigns_removed_from(availability) {
+	// returns a string with the names of campaign(s) the user will be removed from, based on their new availability selections on the page.
+	// if there are none the user will be removed from, returns an empty string
+	// ex: "<CampaignName1>\n<CampaignName2>\n" or ""
+	// availability parameter is a JSON object with their availability selections after they hit submit
+	// ex: {monday:1, tuesday:0, ...}
 	
-	if (document.getElementById(day).checked == true) {available = "1"}
-	else {available = "0"}
+	var campaigns_removed_from = "";
+	var table_body = document.getElementById("campaigns-availability-table-body");
+	var r = 0;
+	while (row=table_body.rows[r++]) {
+		if (availability[row.cells[1].innerText.toLowerCase()] == 0) {
+			campaigns_removed_from = campaigns_removed_from + row.cells[0].innerText + "\n";
+		}
+	}
+	return campaigns_removed_from;
+}
+
+function update(availability) {
+	// Sends a POST request with the user's new availability.
+	// (Removing the user from any/applicable campaigns is handled by the backend.)
 	
-	// create query string? Nah, POST request can handle that, I don't think this is needed.
-	// UPDATE <tablename> WHERE user_id = " + user_id_string + " SET " + day + " = " + available;
-	
-	// On getting a response display a message to the user (update successful).
-	
-	// POST Request to Update Day Available
 	var req = new XMLHttpRequest();
-	var payload = {username, day:day_, available};
-	req.open('POST', 'http://httpbin.org/post', true);
+	var payload = availability;
+	req.open('POST', get_post_url(), true);
 	req.setRequestHeader('Content-Type', 'application/json');
 	req.addEventListener('load',function(){
-	  if(req.status >= 200 && req.status < 400){
-		console.log('request received')
-		console.log(req.response)
-		var response = JSON.parse(req.responseText);
-		console.log(response)
-		document.getElementById('availability_results').textContent = response.success;
-	  } else {
-		console.log("Error in network request: " + req.statusText);
-	  }});
+		if(req.status >= 200 && req.status < 400){
+			console.log('success!');
+		} else {
+			console.log("Error in network request: " + req.statusText);
+			alert("error in network request.");
+		}
+		document.location.reload();
+		//alert("availability updated");
+	});
 	req.send(JSON.stringify(payload));
-	event.preventDefault();
-	
 }
+
+function availability_submit_listener() {
+	// Submits the user's new availability selections for UPDATEing.
+	// We do not traingulate the delta, simply grab all values and send a POST request.
+	// If the user has campaigns they will be removed from because they marked themselves as unnavailable, they are prompted and have the option to cancel.
+	
+	event.preventDefault();
+	var availability = get_availability();
+	var removed_from = get_campaigns_removed_from(availability);
+	
+	// if they have any campaigns they will be removed from, prompt them and give option to cancel
+	if (removed_from != "") {
+		var con_tinue = confirm("Given your availability updates, you will be removed from the following campaigns:\n\n" + removed_from + "\nDo you want to continue?");
+		if (con_tinue == true) {
+			update(availability);
+		}
+		else {
+			// user clicked cancel, do nothing.
+		}
+	}
+	// no campaigns they will be removed from, update their availability
+	else {
+		update(availability);
+	}
+}
+
+window.addEventListener('DOMContentLoaded', function () {
+	document.getElementById("availability-submit-button").addEventListener("click", function(){
+		event.preventDefault();
+		availability_submit_listener();
+	});
+});
